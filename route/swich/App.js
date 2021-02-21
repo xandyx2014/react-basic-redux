18b6eb9b60d267b83bad89c42db3e5feb26a3dac
xandyx2014 <xandyx2014@gmail.com>
2021-02-21 14:02:32 -0400

    nuevos temas

--- route/swich/App.js
+++ route/swich/App.js
@@ -14,10 +14,13 @@ const Playlist = () => (
 )
 
 const App = () => {
+  // Switch rederiza la primera ruta q encuentra y no rederiza los 2 componentes videos
+  // en este caso solo renderizaria videos ya que esta como primero en la fila
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/' exact render={Home} />
+        { {/* SOlO ESTA RUTA SE RENDERIZARA PORQ ES LA PRIMERA */}}
         <Route path='/videos' render={Videos} />
         <Route path='/videos' render={Playlist} />
       </Switch>
